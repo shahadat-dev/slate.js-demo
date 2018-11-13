e39083ca7f507daf3d788229e39b9c7e29f05752
Shahadat Hossain <shahadat.cseru@gmail.com>
2018-11-14 02:36:23 +0600

    added toolbar,ul,ol

---
+++ src/components/FormatToolbar.js
@@ -0,0 +1,7 @@
+import React, { Component } from 'react'
+
+const FormatToolbar = props => (
+  <div className="format-toolbar">{props.children}</div>
+)
+
+export default FormatToolbar
--- src/components/TextEditor.js
+++ src/components/TextEditor.js
@@ -1,7 +1,13 @@
-import React, { Component } from 'react'
+import React, { Component, Fragment } from 'react'
 import { Editor } from 'slate-react'
 import { Value } from 'slate'
-import { BoldMark, ItalicMark } from './index'
+
+import Icon from 'react-icons-kit'
+import { bold } from 'react-icons-kit/feather/bold'
+import { italic } from 'react-icons-kit/feather/italic'
+import { list } from 'react-icons-kit/feather/list'
+
+import { BoldMark, ItalicMark, FormatToolbar } from './index'
 
 // Create our initial value...
 const initialValue = Value.fromJSON({
@@ -30,13 +36,18 @@ class TextEditor extends Component {
     value: initialValue
   }
 
+  // Store a reference to the `editor`.
+  ref = editor => {
+    this.editor = editor
+  }
+
   // On change, update the app's React state with the new editor value.
   onChange = ({ value }) => {
     this.setState({ value })
   }
 
   // On Key Down
-  onKeyDown = (event, change, next) => {
+  onKeyDown = (event, editor, next) => {
     // Return with no changes if the keypress is not 'Ctrl'
     if (!event.ctrlKey) {
       return next()
@@ -47,12 +58,12 @@ class TextEditor extends Component {
     switch (event.key) {
       // When 'b' is pressed, add a 'bold' mark to the text
       case 'b': {
-        change.toggleMark('bold')
+        editor.toggleMark('bold')
         return true
       }
       // When 'i' is pressed, add a 'italic' mark to the text
       case 'i': {
-        change.toggleMark('italic')
+        editor.toggleMark('italic')
         return true
       }
       default: {
@@ -69,20 +80,80 @@ class TextEditor extends Component {
       case 'italic':
         return <ItalicMark {...props} />
 
+      case 'ul':
+        return (
+          <ul {...props.attributes}>
+            <li>{props.children}</li>
+          </ul>
+        )
+
+      case 'ol':
+        return (
+          <ol {...props.attributes}>
+            <li>{props.children}</li>
+          </ol>
+        )
+
       default:
         return next()
     }
   }
 
-  // Render the editor.
+  onMarkClick = (event, type) => {
+    event.preventDefault()
+    // console.log(event, type)
+    const { editor } = this
+    const { value } = editor
+    const change = editor.toggleMark(type)
+
+    console.log(this, editor, value, change)
+
+    this.onChange(change)
+  }
+
   render() {
     return (
-      <Editor
-        value={this.state.value}
-        onChange={this.onChange}
-        onKeyDown={this.onKeyDown}
-        renderMark={this.renderMark}
-      />
+      <Fragment>
+        <FormatToolbar>
+          <button
+            onPointerDown={event => this.onMarkClick(event, 'bold')}
+            className="tooltip-icon-button"
+          >
+            <Icon icon={bold} />
+          </button>
+
+          <button
+            onPointerDown={event => this.onMarkClick(event, 'italic')}
+            className="tooltip-icon-button"
+          >
+            <Icon icon={italic} />
+          </button>
+
+          <button
+            onPointerDown={event => this.onMarkClick(event, 'ul')}
+            className="tooltip-icon-button"
+          >
+            <Icon icon={list} />
+            ul
+          </button>
+
+          <button
+            onPointerDown={event => this.onMarkClick(event, 'ol')}
+            className="tooltip-icon-button"
+          >
+            <Icon icon={list} />
+            ol
+          </button>
+        </FormatToolbar>
+
+        <Editor
+          ref={this.ref}
+          value={this.state.value}
+          onChange={this.onChange}
+          onKeyDown={this.onKeyDown}
+          renderMark={this.renderMark}
+        />
+      </Fragment>
     )
   }
 }
