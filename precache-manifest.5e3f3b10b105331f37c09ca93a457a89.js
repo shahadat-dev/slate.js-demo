be0e5dddfc6329f03781d0b92d5701371db01bd0
Shahadat Hossain <shahadat.cseru@gmail.com>
2018-11-15 01:49:02 +0600

    Updates

---
+++ precache-manifest.5e3f3b10b105331f37c09ca93a457a89.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "22c03900261237585479",
+    "url": "/slate.js-demo/static/js/runtime~main.22c03900.js"
+  },
+  {
+    "revision": "73ab90861f2c2555a363",
+    "url": "/slate.js-demo/static/js/main.73ab9086.chunk.js"
+  },
+  {
+    "revision": "f63adaaed6b5e094de18",
+    "url": "/slate.js-demo/static/js/1.f63adaae.chunk.js"
+  },
+  {
+    "revision": "73ab90861f2c2555a363",
+    "url": "/slate.js-demo/static/css/main.82314b83.chunk.css"
+  },
+  {
+    "revision": "8cafe33f195b82adab43adc7c7eaf1f1",
+    "url": "/slate.js-demo/index.html"
+  }
+];
